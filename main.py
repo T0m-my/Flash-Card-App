b13d4064b246c85020439dd0b57aa0dee63a920c
Thomas Mumena <mumenathomas@gmail.com>
2024-02-25 12:41:39 +0200

    Create a new flash card with the press of the buttons.The words the user knows Should not appear again, and when the program is rerun, it should remember unless it's run for the first time.

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
 from tkinter import *
-# import pandas
+import pandas
+from random import choice
 
 # CONSTANTS
 BACKGROUND_COLOR = "#B1DDC6"
+DELAY_MS = 3000
+card = {}
+timer = ''
+data = {}
+
+# create a list of dictionaries from the csv file
+try:
+    df_to_learn = pandas.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    df = pandas.read_csv('./data/french_words.csv')
+    data = df.to_dict(orient='records')
+else:
+    data = df_to_learn.to_dict(orient='records')
+
+
+def count_down():
+    global timer
+    timer = window.after(DELAY_MS, flip_card)
+
+
+def flip_card():
+    window.after_cancel(timer)
+    canvas.itemconfig(card_background, image=card_back_img)
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(card_word, text=card['English'], fill='white')
+
+
+def next_card():
+    global card
+    card = choice(data)
+
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_word, text=card['French'], fill='black')
+    canvas.itemconfig(card_background, image=card_front_img)
+
+    count_down()
+
+
+def remove_word():
+    words_to_learn = data.remove(card)
+    data_frame = pandas.DataFrame(words_to_learn)
+    data_frame.to_csv('./data/words_to_learn.csv', index=False)
+
+    next_card()
+
 
 # UI Setup
 window = Tk()
@@ -12,18 +58,20 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file='./images/card_front.png')
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file='./images/card_back.png')
+card_background = canvas.create_image(400, 263, image=card_front_img)
 canvas.config(highlightthickness=0, bg=BACKGROUND_COLOR)
-title_text = canvas.create_text(400, 150, text='Title', fill='black', font=('Arial', 40, 'italic'))
-word_text = canvas.create_text(400, 263, text='Word', fill='black', font=('Arial', 60, 'bold'))
+card_title = canvas.create_text(400, 150, text='', fill='black', font=('Arial', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='', fill='black', font=('Arial', 60, 'bold'))
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_img = PhotoImage(file='./images/right.png')
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=remove_word)
 right_button.grid(column=1, row=1)
 
 wrong_img = PhotoImage(file='./images/wrong.png')
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
+next_card()
 window.mainloop()
